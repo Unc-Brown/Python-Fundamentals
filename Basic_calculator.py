5196914d16409aafab2bb66e9d0cbc58c794c0b8
Uncle_Fr <talktofrank08@gmail.com>
2026-02-04 21:21:13 +0100

    Update Basic_calculator.py

--- Basic_calculator.py
+++ Basic_calculator.py
@@ -14,22 +14,23 @@ elif a == "-":
     if diff<0:
         print("This is a negative number")
         b = input("Do you wish to switch/change values? (Yes/No): ")
-        if b == "Yes" or b == "yes":
+        if b == "Yes".lower():
             c = float(input("First number: "))
             d = float(input("Second number: "))
             e = c - d
             print(f"New Difference = {e}")
-            if e < 0:
+            while e < 0:
                 print("This is still a negative number")
                 x = input("Do you still wish to switch/change values again? (Yes/No): ")
-                if x == "Yes" or x == "yes":
+                if x == "Yes".lower():
                     c = float(input("First number: "))
                     d = float(input("Second number: "))
                     e = c - d
                     print(f"New Difference = {e}")
                 elif x == "No" or x == "no":
                     print("Alright then, there you have it!")
-        elif b == "No" or b == "no":
+                    break
+        elif b == "No".lower():
             print("Okay then!")
 elif a == "*":
     num1 = float(input("First number: "))
@@ -39,9 +40,21 @@ elif a == "*":
 elif a == "/":
     num1 = float(input("First number: "))
     num2 = float(input("Second number: "))
-    division = num1/num2
-    modulus = num1%num2
-    print(f"{num1}/{num2} = {round(division)} remainder {modulus}")
+    while num2 == 0:
+        print(f"{num1} can not be divided by 0")
+        query = input("Do you wish to change numbers: ")
+        if query == "Yes".lower():
+            num1_ = float(input("First number: "))
+            num2_ = float(input("Second number: "))
+            division = num1_/num2_
+            modulus = num1_%num2_
+            if not modulus == 0:
+                print(f"{num1_}/{num2_} = {round(division)} remainder {modulus}")
+            else:
+                print(f"{num1_}/{num2_} = {round(division)}")
+        if query == "No".lower():
+            print(f"Error: {num1} is not divisible by 0")
+        break
 else:
     print("Invalid Operator!")
     print("Use a valid operator.")
